5d8792f0ada0f6f0f8c3f49394b95a36d09a7cf0
Aashish Jha <aashishjha1107@gmail.com>
2025-02-24 09:46:39 +0530

    Delete src/NoTaskFound.js

--- src/NoTaskFound.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-
-const NoTaskFound = () => {
-    return (
-        <div className=" flex justify-center p-4"><p className="p-2 bg-yellow-400 rounded-xl font-semibold text-xl ">No Task Found</p></div>)
-}
-
-export default NoTaskFound
